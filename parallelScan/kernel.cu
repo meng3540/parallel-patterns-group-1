25c21031c98dce904b1ec31a5bf5ee43901a7887
Fishtaco2006 <124905908+Fishtaco2006@users.noreply.github.com>
2025-03-11 16:31:41 -0400

    Update kernel.cu

--- parallelScan/kernel.cu
+++ parallelScan/kernel.cu
@@ -1,1 +1,25 @@
+__global__ void Kogge_Stone_scan_kernel(float *X, float *Y, unsigned int N) {
+    __shared__ float XY[SECTION_SIZE]; 
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(i < N) {
+        XY[threadIdx.x] = X[i];
+    } else {
+        XY[threadIdx.x] = 0.0f;
+    }
+
+    for(unsigned int stride = 1; stride < blockDim.x; stride *= 2) {
+        __syncthreads();
+        float temp;
+        if(threadIdx.x >= stride)
+            temp = XY[threadIdx.x] + XY[threadIdx.x - stride];
+        __syncthreads();
+        if(threadIdx.x >= stride)
+            XY[threadIdx.x] = temp;
+    }
+
+    if(i < N) {
+        Y[i] = XY[threadIdx.x];
+    }
+}
 ﻿
